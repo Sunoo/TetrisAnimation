d3653caddd4cf8b4720adfe5cfdcdbb0f1b2f674
David Maher <sunookitsune@kitsunet.org>
2019-07-05 20:17:46 -0400

    Added command line switches.

--- examples/Clock.cpp
+++ examples/Clock.cpp
@@ -8,21 +8,73 @@
 using namespace rgb_matrix;
 using std::chrono::system_clock;
 
-int main(int argc, char *argv[]) {
-  RGBMatrix::Options matrix_options;
-  matrix_options.rows = 16;
-  rgb_matrix::RuntimeOptions runtime_opt;
-  rgb_matrix::ParseOptionsFromFlags(&argc, &argv, &matrix_options, &runtime_opt);
+static int usage(const char *progname) {
+  fprintf(stderr, "usage: %s [options]\n", progname);
+  fprintf(stderr, "Displays the time as a series of falling Tetris blocks.\n");
+  fprintf(stderr, "Options:\n");
+  rgb_matrix::PrintMatrixFlags(stderr);
+  fprintf(stderr,
+          "\t-b <brightness>   : Sets brightness percent. Default: 100.\n"
+          "\t-x <x>            : Starting X position of displayed time. Default: 1\n"
+          "\t-y <yFinish>      : Ending Y position of displayed time. Default: 16\n"
+          "\t-0                : Show leading zeros in the hour.\n"
+          "\t-t                : Use 24-hour clock.\n"
+          "\t-r                : Force refresh of all digits every minute.\n"
+          "\t-d                : Use double size blocks.\n"
+          );
+  return 1;
+}
 
-  //const char *time_format = "%H:%M";
-  const char *time_format = "%I:%M";
-  
+int main(int argc, char *argv[]) {
   int brightness = 100;
+  int x = 1;
+  int yFinish = 16;
+  bool leadingZero = false;
+  char *time_format = "%I:%M";
+  bool forceRefresh = false;
+  int scale = 1;
+  bool drawOutline = false;
+  Color outLineColour = Color(0, 0, 0);
+
+  int opt;
+  while ((opt = getopt(argc, argv, "b:x:y:0trd")) != -1) {
+    switch (opt) {
+      case 'b':
+        brightness = atoi(optarg);
+        break;
+      case 'x':
+        x = atoi(optarg);
+        break;
+      case 'y':
+        yFinish = atoi(optarg);
+        break;
+      case '0':
+        leadingZero = true;
+        break;
+      case 't':
+        time_format = "%H:%M";
+        break;
+      case 'r':
+        forceRefresh = true;
+        break;
+      case 'd':
+        scale = 2;
+        break;
+      break;
+      default:
+        return usage(argv[0]);
+    }
+  }
 
   if (brightness < 1 || brightness > 100) {
     fprintf(stderr, "Brightness is outside usable range.\n");
     return 1;
   }
+
+  RGBMatrix::Options matrix_options;
+  matrix_options.rows = 16;
+  rgb_matrix::RuntimeOptions runtime_opt;
+  rgb_matrix::ParseOptionsFromFlags(&argc, &argv, &matrix_options, &runtime_opt);
 
   RGBMatrix *canvas = rgb_matrix::CreateMatrixFromOptions(matrix_options, runtime_opt);
   if (canvas == NULL)
@@ -33,6 +85,7 @@ int main(int argc, char *argv[]) {
   FrameCanvas *offscreen = canvas->CreateFrameCanvas();
   
   TetrisMatrixDraw tetris(*offscreen);
+  tetris.scale = scale;
 
   while (true) {
       std::time_t tt = system_clock::to_time_t(system_clock::now());
@@ -41,17 +94,17 @@ int main(int argc, char *argv[]) {
       ss << std::put_time(ptm, time_format);
       string time = ss.str();
       
-      if (time[0] == '0')
+      if (!leadingZero && time[0] == '0')
       {
         time[0] = ' ';
       }
       
-      tetris.setTime(time);
+      tetris.setTime(time, forceRefresh);
       bool finished = false;
       while (!finished)
       {
         offscreen->Clear();
-        finished = tetris.drawNumbers(1, 16, true);
+        finished = tetris.drawNumbers(x, yFinish, true);
 
         usleep(100000);
         canvas->SwapOnVSync(offscreen);
